618a5682a282bdf97eba798f05a2ab0300a90bba
Dan6erbond <moravrav@gmail.com>
2020-07-04 09:08:26 +0200

    Update tests Write tests test_update_joined and test_update_unique

--- tests/test_reactive.py
+++ tests/test_reactive.py
@@ -1,8 +1,10 @@
+from typing import Any
+
 from reactive import ReactiveOwner, ReactiveProperty
 
 
 class TestReactive:
-    def test_reactive(self):
+    def test_update_joined(self):
         class Foo(ReactiveOwner):
             def __init__(self):
                 super().__init__()
@@ -10,15 +12,42 @@ class TestReactive:
                 self.age = ReactiveProperty(6)
                 self.is_reactive = ReactiveProperty(True)
 
+        update_name_joined_called = False
+        update_age_joined_called = False
+
+        def call_update_joined(val: Any):
+            if val.name == "name":
+                update_name_joined_called = True
+            elif val.name == "age":
+                update_age_joined_called = True
+
         foo = Foo()
-        foo.on_update(
-            lambda x: print(
-                "Updated value:",
-                x.name,
-                "To:",
-                x),
-            foo.name,
-            foo.age)
-        foo.name.on_update(lambda x, y: print("New value:", x))
+
+        foo.on_update(call_update_joined, foo.name, foo.age)
+
         foo.name = "Bar"
         foo.age = 12
+
+        assert update_name_joined_called
+        assert update_age_joined_called
+
+    def test_update_unique(self):
+        class Foo(ReactiveOwner):
+            def __init__(self):
+                super().__init__()
+                self.name = ReactiveProperty("Foo")
+                self.age = ReactiveProperty(6)
+                self.is_reactive = ReactiveProperty(True)
+
+        update_name_unique_called = False
+
+        def call_update_name(curr: Any, old: Any):
+            update_name_unique_called = True
+
+        foo = Foo()
+
+        foo.name.on_update(call_update_name)
+
+        foo.name = "Bar"
+
+        assert update_name_unique_called
