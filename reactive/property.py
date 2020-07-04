204aa87e472294cbbdc345080f9ef0de452342f8
Dan6erbond <moravrav@gmail.com>
2020-07-04 16:13:33 +0200

    Enable multiple change handlers for properties

--- reactive/property.py
+++ reactive/property.py
@@ -6,20 +6,22 @@ class ReactiveProperty:
         self.value = value
         self.name = name
         self.history = [value]
-        self._on_change = None
+        self._on_change_handlers = []
 
     def __get__(self, instance: Any, owner: Any):
         class ObservableProperty(type(self.value)):
             name = self.name
             history = self.history
             on_change = self.on_change
+
         return ObservableProperty(self.value)
 
     def __set__(self, instance: Any, value: Any):
         prev = self.value
         self.value = value
-        if self._on_change and value != prev:
-            self._on_change(self.value, prev)
+        if value != prev:
+            for change_handler in self._on_change_handlers:
+                change_handler(self.value, prev)
 
     def __delete__(self, instance: Any):
         del self.value
@@ -28,4 +30,4 @@ class ReactiveProperty:
         self.name = name
 
     def on_change(self, func: Callable[[Any, Any], None]):
-        self._on_change = func
+        self._on_change_handlers.append(func)
