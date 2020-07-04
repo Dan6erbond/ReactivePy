02394f7101426bf2e0d8af4cdec09a095892ac4f
Dan6erbond <moravrav@gmail.com>
2020-07-04 10:05:39 +0200

    Add _bulk_update in `class ReactiveOwner`

--- reactive/owner.py
+++ reactive/owner.py
@@ -17,6 +17,32 @@ class ReactiveOwner:
             return obj.__get__(self, type(self))
         return obj
 
+    def _bulk_update(self, *args):
+        funccalls = {}
+
+        for arg in args:
+            try:
+                obj = object.__getattribute__(self, arg["name"])
+            except AttributeError:
+                object.__setattr__(self, arg["name"], arg["value"])
+            else:
+                if hasattr(obj, '__set__'):
+                    obj.__set__(self, arg["value"])
+
+                    if isinstance(obj, ReactiveProperty):
+                        for update in self._on_updates:
+                            if obj in update[1]:
+                                func = update[0]
+                                if func in funccalls:
+                                    funccalls[func] = funccalls[func] + [obj]
+                                else:
+                                    funccalls[func] = [obj]
+                else:
+                    object.__setattr__(self, arg["name"], arg["value"])
+
+        for func in funccalls:
+            func(*funccalls[func])
+
     def __setattr__(self, name: str, value: Any):
         try:
             obj = object.__getattribute__(self, name)
@@ -31,6 +57,6 @@ class ReactiveOwner:
                 if isinstance(obj, ReactiveProperty):
                     for update in self._on_updates:
                         if obj in update[1]:
-                            update[0](obj)
+                            update[0](*[obj])
             else:
                 object.__setattr__(self, name, value)
