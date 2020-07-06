f4cf74eb94fe6e31674e29bbde9c8218803b19a5
Dan6erbond <moravrav@gmail.com>
2020-07-06 08:57:13 +0200

    Add `to_tuple`
    Convert lists, single types to `tuple`.

--- reactive/owner.py
+++ reactive/owner.py
@@ -3,6 +3,17 @@ from typing import Any, Callable
 from .property import ReactiveProperty
 
 
+def to_tuple(val):
+    if isinstance(val, list):
+        return tuple(val)
+    elif isinstance(val, tuple):
+        return val
+    elif val is None:
+        return val
+    else:
+        return (val)
+
+
 def all_reactive(org_cls=None, only_type=None, not_type=None):
     if org_cls:
         class AllReactive(org_cls, ReactiveOwner):
@@ -29,8 +40,8 @@ def all_reactive(org_cls=None, only_type=None, not_type=None):
         class AllReactive(org_cls, ReactiveOwner):
             def __init__(self):
                 super().__init__()
-                self.__only_type = tuple(only_type) if only_type else only_type
-                self.__not_type = tuple(not_type) if not_type else not_type
+                self.__only_type = to_tuple(only_type)
+                self.__not_type = to_tuple(not_type)
 
             def __setattr__(self, name: str, value: Any):
                 if not isinstance(value, ReactiveProperty) and not name.startswith("_"):
