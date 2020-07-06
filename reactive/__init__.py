8ec869e811892a8a12fae63cbaec39dc531c69b6
Dan6erbond <moravrav@gmail.com>
2020-07-06 15:00:14 +0200

    Move all_reactive to separate file

--- reactive/__init__.py
+++ reactive/__init__.py
@@ -1,3 +1,4 @@
+from .all_reactive import all_reactive
 from .const import __tag__, __version__
-from .owner import ReactiveOwner, all_reactive
+from .owner import ReactiveOwner
 from .property import ReactiveProperty
--- reactive/owner.py
+++ reactive/owner.py
@@ -3,63 +3,6 @@ from typing import Any, Callable
 from .property import ReactiveProperty
 
 
-def to_tuple(val):
-    if isinstance(val, list):
-        return tuple(val)
-    elif isinstance(val, tuple):
-        return val
-    elif val is None:
-        return val
-    else:
-        return (val)
-
-
-def all_reactive(org_cls=None, only_type=None, not_type=None):
-    if org_cls:
-        class AllReactive(org_cls, ReactiveOwner):
-            def __init__(self):
-                super().__init__()
-                self.__only_type = only_type
-                self.__not_type = not_type
-
-            def __setattr__(self, name: str, value: Any):
-                if not isinstance(value, ReactiveProperty) and not name.startswith("_"):
-                    if self.__only_type and not isinstance(value, self.__only_type):
-                        return super().__setattr__(name, value)
-                    if self.__not_type and isinstance(value, self.__not_type):
-                        return super().__setattr__(name, value)
-                    try:
-                        value = ReactiveProperty(value, name)
-                    except TypeError:
-                        pass
-                return super().__setattr__(name, value)
-
-        return AllReactive
-
-    def all_reactive_wrapper(org_cls):
-        class AllReactive(org_cls, ReactiveOwner):
-            def __init__(self):
-                super().__init__()
-                self.__only_type = to_tuple(only_type)
-                self.__not_type = to_tuple(not_type)
-
-            def __setattr__(self, name: str, value: Any):
-                if not isinstance(value, ReactiveProperty) and not name.startswith("_"):
-                    if self.__only_type and not isinstance(value, self.__only_type):
-                        return super().__setattr__(name, value)
-                    if self.__not_type and isinstance(value, self.__not_type):
-                        return super().__setattr__(name, value)
-                    try:
-                        value = ReactiveProperty(value, name)
-                    except TypeError:
-                        pass
-                return super().__setattr__(name, value)
-
-        return AllReactive
-
-    return all_reactive_wrapper
-
-
 class ReactiveOwner:
     def __init__(self):
         self.__on_change_handlers = []
